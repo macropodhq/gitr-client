1b1f8755b650e67a970a571f2387888aadf53136
Matt Milosavljevic <matt@bugherd.com>
2014-12-23 12:05:21 +1100

    messages now look respectable

--- app/views/messages.jsx
+++ app/views/messages.jsx
@@ -6,6 +6,7 @@ var Router = require('react-router');
 var FluxMixin = Fluxxor.FluxMixin(React);
 var StoreWatchMixin = Fluxxor.StoreWatchMixin;
 var Wrapper = require('./wrapper');
+var Icon = require('../base/components/icon');
 
 var log = require('bows')('Messages View');
 
@@ -30,12 +31,42 @@ var Messages = module.exports = React.createClass({
           <ul className="Messages-users">
             <li>
               <Router.Link to="thread" params={{userId: 1}}>
+                <Icon type="nav-right" font={false} />
                 <img src="https://avatars1.githubusercontent.com/u/479055" />
                 <h2>Conrad Pancake</h2>
               </Router.Link>
             </li>
             <li>
               <Router.Link to="messages">
+                <Icon type="nav-right" font={false} />
+                <img src="https://avatars0.githubusercontent.com/u/33324" />
+                <h2>Nathan coffeeface</h2>
+              </Router.Link>
+            </li>
+            <li>
+              <Router.Link to="messages">
+                <Icon type="nav-right" font={false} />
+                <img src="https://avatars0.githubusercontent.com/u/33324" />
+                <h2>Nathan coffeeface</h2>
+              </Router.Link>
+            </li>
+            <li>
+              <Router.Link to="messages">
+                <Icon type="nav-right" font={false} />
+                <img src="https://avatars0.githubusercontent.com/u/33324" />
+                <h2>Nathan coffeeface</h2>
+              </Router.Link>
+            </li>
+            <li>
+              <Router.Link to="messages">
+                <Icon type="nav-right" font={false} />
+                <img src="https://avatars0.githubusercontent.com/u/33324" />
+                <h2>Nathan coffeeface</h2>
+              </Router.Link>
+            </li>
+            <li>
+              <Router.Link to="messages">
+                <Icon type="nav-right" font={false} />
                 <img src="https://avatars0.githubusercontent.com/u/33324" />
                 <h2>Nathan coffeeface</h2>
               </Router.Link>
