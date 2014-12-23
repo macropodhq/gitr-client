a382e74db1783b3c5c2edbfa1566a881926016a8
James Coleman <james.coleman@bugherd.com>
2014-12-23 11:39:26 +1100

    Use swipe on details page

--- app/views/detail.jsx
+++ app/views/detail.jsx
@@ -2,6 +2,7 @@
 
 var React = require('react');
 var Fluxxor = require('fluxxor');
+var Swipe = require('react-swipe/swipe');
 var FluxMixin = Fluxxor.FluxMixin(React);
 var StoreWatchMixin = Fluxxor.StoreWatchMixin;
 var Wrapper = require('./wrapper');
@@ -23,10 +24,20 @@ var Detail = module.exports = React.createClass({
   },
 
   render() {
+
+    var panes = Array.apply(null, Array(20)).map(function(_, i) {
+      return React.DOM.div({key: i}, React.DOM.b(null, i))
+    })
+
     return (
       <Wrapper leftLink={{to: 'swipe', iconType: 'nav-left'}} rightLink={{to: 'messages', iconType: 'bubble'}}>
         <div className="Detail">
           <h1>Conrad Pancake</h1>
+          <Swipe id="Swipez">
+            <div>asdfasdf</div>
+            <div>asdfasdf 3</div>
+            <div>asdfasdf 6</div>
+          </Swipe>
         </div>
       </Wrapper>
     );
