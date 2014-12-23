74284c241b9d03fa371e8dc974c5430179cf0663
James Coleman <james.coleman@bugherd.com>
2014-12-23 15:47:09 +1100

    Actually have repos in the details page

--- app/views/person.jsx
+++ app/views/person.jsx
@@ -18,12 +18,15 @@ var Person = module.exports = React.createClass({
 
   mixins: [
     FluxMixin,
-    StoreWatchMixin('PersonStore'),
+    StoreWatchMixin('SuggestionStore'),
     Router.State,
   ],
 
   getStateFromFlux() {
-    return {};
+    var SuggestionStore = this.getFlux().store('SuggestionStore');
+    return {
+      repos: SuggestionStore.get(this.getParams().id).repos
+    };
   },
 
   getInitialState() {
@@ -42,81 +45,44 @@ var Person = module.exports = React.createClass({
     })
   },
 
+  work(repo) {
+    console.log(repo)
+    return (
+      <div className="Detail-work">
+        <div className="Detail-work-inside">
+          <h2>{repo.name}</h2>
+          <div className="Detail-work-iconography">
+            <div className="Detail-work-icon">
+              <Icon type="eye" font={false} /> {repo.watchers}
+            </div>
+            <div className="Detail-work-icon">
+              <Icon type="share" font={false} /> {repo.forks}
+            </div>
+          </div>
+          <p>{repo.description}</p>
+        </div>
+      </div>
+    );
+  },
+
   render() {
     var panes = Array.apply(null, Array(20)).map(function(_, i) {
       return React.DOM.div({key: i}, React.DOM.b(null, i))
     });
 
-    console.log(this.getParams())
-
-    var personStore = this.getFlux().store('PersonStore');
-    console.log(personStore.getBy({id: this.getParams().id}))
-
-    var repos = [1,2,3];
-
     return (
       <Wrapper leftLink={{to: 'swipe', iconType: 'nav-left'}} rightLink={{to: 'matches', iconType: 'bubbles'}} heading="Conrad">
         <div className="Detail">
           <Swipe id="Detail-portfolio" callback={this.handleSwipe}>
-            <div className="Detail-work">
-              <div className="Detail-work-inside">
-                <h2>npmrc</h2>
-                <div className="Detail-work-iconography">
-                  <div className="Detail-work-icon">
-                    <Icon type="star" font={false} /> 13
-                  </div>
-                  <div className="Detail-work-icon">
-                    <Icon type="share" font={false} /> 2
-                  </div>
-                  <div className="Detail-work-icon">
-                    <Icon type="users" font={false} /> 0
-                  </div>
-                </div>
-                <p>Switch between different .npmrc files with ease and grace.</p>
-              </div>
-            </div>
-            <div className="Detail-work">
-              <div className="Detail-work-inside">
-                <h2>2</h2>
-                <div className="Detail-work-iconography">
-                  <div className="Detail-work-icon">
-                    <Icon type="star" font={false} /> 13
-                  </div>
-                  <div className="Detail-work-icon">
-                    <Icon type="share" font={false} /> 2
-                  </div>
-                  <div className="Detail-work-icon">
-                    <Icon type="users" font={false} /> 0
-                  </div>
-                </div>
-                <p>Switch between different .npmrc files with ease and grace.</p>
-              </div>
-            </div>
-            <div className="Detail-work">
-              <div className="Detail-work-inside">
-                <h2>3</h2>
-                <div className="Detail-work-iconography">
-                  <div className="Detail-work-icon">
-                    <Icon type="star" font={false} /> 13
-                  </div>
-                  <div className="Detail-work-icon">
-                    <Icon type="share" font={false} /> 2
-                  </div>
-                  <div className="Detail-work-icon">
-                    <Icon type="users" font={false} /> 0
-                  </div>
-                </div>
-                <p>Switch between different .npmrc files with ease and grace.</p>
-              </div>
-            </div>
+            { this.state.repos.map(function(repo) {
+                return (
+                  this.work(repo)
+                )
+              }.bind(this))
+            }
           </Swipe>
           <div className="Detail-portfolio-pagination">
-            { repos.map(function(repo, index) {
-              console.log('rendering')
-              console.log({
-                index: index,
-                currentIndex: this.state.slide.currentIndex
-              })
+            { this.state.repos.map(function(repo, index) {
                 return (
                   <span className={'Detail-portfolio-pagination-page' + (this.state.slide.currentIndex === index ? ' Detail-portfolio-pagination-page--active' : ' ')}></span>
                 )
