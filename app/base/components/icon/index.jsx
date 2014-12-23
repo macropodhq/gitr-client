39c45700a0065860a80994f2d8a24a6201ccfd0d
James Coleman <james.coleman@bugherd.com>
2014-12-24 09:17:05 +1100

    Drop the font icons on the head

--- app/base/components/icon/index.jsx
+++ app/base/components/icon/index.jsx
@@ -1,8 +1,6 @@
 /** @jsx React.DOM */
 
 var React = require('react/addons');
-require('./icon.scss');
-
 var Icon = React.createClass({
   getDefaultProps: function() {
     return {
@@ -12,18 +10,12 @@ var Icon = React.createClass({
   },
 
   componentDidMount: function() {
-    if (!this.props.font) {
-      this.getDOMNode().innerHTML = require('!raw!./svgs/icon-' + this.props.type + '.svg');
-    }
+    this.getDOMNode().innerHTML = require('!raw!./svgs/icon-' + this.props.type + '.svg');
   },
 
   render: function() {
     var component = this.props.component || React.DOM.i;
     var props = {className: 'Icon'};
-
-    if (this.props.font) {
-      props['data-am-icon'] = this.props.type;
-    }
 
     return this.transferPropsTo(component(props));
   }
